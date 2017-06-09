e39e4e023d55c18bb94edf65a0a8a27f004a5139
Preethi Kasireddy <iam.preethi.k@gmail.com>
2017-06-09 08:47:16 -0700

    Add notes for how the gradient loss function works in SVM

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -35,6 +35,21 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        # gradient with respect to wyi: count the number of classes that didn't 
+        # meet the desired margin (and hence contributed to the loss function) 
+        # and then the data vector xixi scaled by this number is the gradient.
+        
+        # Whenever you compute loss vector for x[i], ith training example and 
+        # get some nonzero loss, that means you should move your weight vector 
+        # for the incorrect class (j != y[i]) away by x[i], and at the same time, 
+        # move the weights or hyperplane for the correct class (j==y[i]) near x[i]. 
+        # By parallelogram law,  w + x lies in between w and x. So this way w[y[i]] 
+        # tries to come nearer to x[i] each time it finds loss>0. 
+        # Thus, dW[:,y[i]] += -X[i] and dW[:,j] += X[i] is done in the loop, 
+        # but while update, we will do in direction of decreasing gradient, 
+        # so we are essentially adding X[i] to correct class weights and 
+        # going away by X[i] from weights that miss classify.
+        
         dW[:, y[i]] += -X[i]
         dW[:, j] += X[i]
 
@@ -77,10 +92,14 @@ def svm_loss_vectorized(W, X, y, reg):
   
   num_classes = W.shape[1]
   num_train = X.shape[0]
-  scores = X.dot(W)
+  scores = X.dot(W) #(N, C)
   
+  # get the correct class scores by indexing (within scores) on the value of the correct class in y
   correct_class_scores = scores[range(num_train), list(y)].reshape(-1,1) # (N, 1)
+  
+  # calculate margin for all class scores
   margins = np.maximum(0, scores - correct_class_scores + 1)
+  # set the margin for the correct class to 0 
   margins[range(num_train), list(y)] = 0
  
   loss = np.sum(margins) / num_train
@@ -104,12 +123,15 @@ def svm_loss_vectorized(W, X, y, reg):
   # loss.                                                                     #
   #############################################################################
   
-  coeff_mat = np.zeros((num_train, num_classes))
-  coeff_mat[margins > 0] = 1
-  coeff_mat[range(num_train), list(y)] = 0
-  coeff_mat[range(num_train), list(y)] = -np.sum(coeff_mat, axis=1)
+  coeff_matrix = np.zeros((num_train, num_classes))
+  # create a binary index of anything which has margin over 0
+  coeff_matrix[margins > 0] = 1
+  # zero out for the actual class values since we don't want to account for them
+  coeff_matrix[range(num_train), list(y)] = 0
+  # a sum over j != y_i
+  coeff_matrix[range(num_train), list(y)] = -np.sum(coeff_matrix, axis=1)
   
-  dW = (X.T).dot(coeff_mat)
+  dW = (X.T).dot(coeff_matrix)
   dW = dW/num_train + reg*W
   #############################################################################
   #                             END OF YOUR CODE                              #
